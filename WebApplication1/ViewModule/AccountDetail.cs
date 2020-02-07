8610d7f4892512da0a5b1b3205999cf34c8c0293
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-02-07 06:51:46 +0300

    Add AccountDetail ViewModule, related to identity API

---
+++ WebApplication1/ViewModule/AccountDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.ViewModule
+{
+    public class AccountDetail
+    {
+        [Required]
+        [MaxLength(12)]
+        [DataType(DataType.Text)]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public bool PresistantLogin { get; set; }
+    }
+}
