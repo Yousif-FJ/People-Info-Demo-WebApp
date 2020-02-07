a7c65c5c7dcaa89f94a300c1e9589c2f721fd341
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-02-07 06:53:19 +0300

    Implement AccountController and configure it , finish implementing Identity API

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.ViewModule;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager , SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public ViewResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(AccountDetail register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(register);
+            }
+            IdentityUser user = new IdentityUser(register.UserName);
+
+            var result = await userManager.CreateAsync(user,register.Password)
+                .ConfigureAwait(true);
+
+            if (!result.Succeeded) 
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty,error.Description);
+                }
+                return View(register);
+            }
+            await signInManager.SignInAsync(user,register.PresistantLogin)
+                .ConfigureAwait(true);
+
+            return RedirectToAction("Index","Home");
+        }
+
+        [HttpGet]
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(AccountDetail login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            var result = await signInManager.PasswordSignInAsync(userName: login.UserName,
+                login.Password, 
+                login.PresistantLogin,
+                lockoutOnFailure: true)
+                .ConfigureAwait(true);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(login);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -30,13 +32,37 @@ namespace WebApplication1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddMvc();
+
+
             services.AddScoped<IPersonRepository, DBRepository>();
             services.AddTransient<IFileRepository, RootFileRepository>();
             //services.AddSingleton<IPersonRepository, MemoryPersonRepository>();
             services.AddDbContextPool<PersonContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("PersonContext")));
             services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<PersonContext>().AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
