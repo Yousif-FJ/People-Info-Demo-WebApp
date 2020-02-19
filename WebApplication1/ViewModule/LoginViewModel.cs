b19f9e4fe31e8e02d3974728d69542b8c396c043
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-02-19 15:47:23 +0300

    Separate Login and Register view model , and Implment remote verification

--- WebApplication1/ViewModule/LoginViewModel.cs
+++ WebApplication1/ViewModule/LoginViewModel.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Controllers;
 
 namespace WebApplication1.ViewModule
 {
-    public class AccountDetail
+    public class LoginViewModel
     {
         [Required]
         [MaxLength(12)]
---
+++ WebApplication1/ViewModule/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Controllers;
+
+namespace WebApplication1.ViewModule
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [MaxLength(12)]
+        [DataType(DataType.Text)]
+        [Remote(nameof(AccountController.IsUserNameAvailable),"Account")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public bool PresistantLogin { get; set; }
+    }
+}
